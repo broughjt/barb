18091f38a69762c05c389e29c32b04075104a71f
Jackson Brough <jacksontbrough@gmail.com>
2024-02-06 00:14:39 -0700

    Mo' multiplicaiton theorems

--- Barb/Data/Integer.lean
+++ Barb/Data/Integer.lean
@@ -113,7 +113,7 @@ theorem add_associative : ∀ (a b c : ℤ), (a + b) + c = a + (b + c) := by
   show ((n + k) + o) + (m + (l + p)) = (n + (k + o)) + ((m + l) + p)
   simp [Natural.add_associative]
 
-theorem add_identity : ∀ (a : ℤ), a + 0 = a := by
+theorem add_zero : ∀ (a : ℤ), a + 0 = a := by
   apply Quotient.ind
   intro (n, m)
   apply Quotient.sound
@@ -250,7 +250,7 @@ theorem multiply_associative : ∀ (a b c : ℤ), (a * b) * c = a * (b * c) := b
     _ = (n*(k*p + l*q) + m*(k*q + l*p)) + g := congrArg (. + _) rfl
     _ = (n*(k*p + l*q) + m*(k*q + l*p)) + ((n*k + m*l)*q + (n*l + m*k)*p) := rfl
 
-theorem multiply_identity : ∀ (a : ℤ), a * 1 = a := by
+theorem multiply_one : ∀ (a : ℤ), a * 1 = a := by
   apply Quotient.ind
   intro (n, m)
   apply Quotient.sound
@@ -298,12 +298,12 @@ theorem right_distributive : ∀ (a b c : ℤ), (a + b) * c = a * c + b * c := b
 instance : CommutativeRing Integer where
   add_commutative := add_commutative
   add_associative := add_associative
-  add_identity := add_identity
+  add_identity := add_zero
   add_inverse := add_inverse
 
   multiply_commutative := multiply_commutative
   multiply_associative := multiply_associative
-  multiply_identity := multiply_identity
+  multiply_identity := multiply_one
 
   left_distributive := left_distributive
   right_distributive := right_distributive
@@ -312,20 +312,118 @@ def subtract (a b : ℤ) : ℤ := a + (-b)
 
 instance : Sub Integer where sub := subtract
 
+theorem negate_equal_of_add_equal_zero {a b : ℤ} (h : a + b = 0) : b = -a := by
+  calc
+    b = b + 0 := (add_zero b).symm
+    _ = b + (a + -a) := congrArg (_ + .) (add_inverse _).symm
+    _ = (b + a) + -a := (add_associative _ _ _).symm
+    _ = (a + b) + -a := congrArg (. + _) (add_commutative _ _)
+    _ = 0 + -a := congrArg (. + _) h
+    _ = -a + 0 := add_commutative _ _
+    _ = -a := add_zero _
+    
+theorem subtract_equal_zero_of_equal {a b : ℤ} (h : a = b) : a - b = 0 := sorry
+
+theorem equal_of_subtract_equal_zero {a b : ℤ} (h : a - b = 0) : a = b := sorry
+
+theorem negate_multiply_equal_negate_multiply (a b : ℤ) : -(a * b) = -a * b := sorry
+
+theorem negate_multiply_equal_multiply_negate (a b : ℤ) : -(a * b) = a * -b := sorry
+
 theorem equal_zero_of_multiply_equal_zero : ∀ {a b : ℤ}, a * b = 0 → a = 0 ∨ b = 0 := by
   apply Quotient.ind₂
-  intro (n, m) (k, l) h
-  apply Quotient.sound
+  intro (n, m) (k, l) h_quotient
+  have h_equivalent : n*k + m*l = n*l + m*k := calc
+    n*k + m*l = (n*k + m*l) + 0 := (Natural.add_zero _).symm
+    _ = 0 + (n*l + m*k) := Quotient.exact h_quotient
+    _ = n*l + m*k := Natural.zero_add _
+  have f {w x y z : ℕ} (h_less : w < x) (h_equivalent : w*y + x*z = w*z + x*y) : (y, z) ≈ (0, 0) := by
+  { let ⟨a, h_positive, (h_exists : w + a = x)⟩ := Natural.equal_add_positive_of_less_than h_less
+    have h₁ := calc
+      (w*y + w*z) + a*z = w*y + (w*z + a*z) := Natural.add_associative _ _ _
+      _ = w*y + (w + a)*z := congrArg (_ + .) (Natural.right_distributive _ _ _).symm
+      _ = w*y + x*z := congrArg (λ x => _ + (x * _)) h_exists
+      _ = w*z + x*y := h_equivalent
+      _ = w*z + (w + a)*y := congrArg (λ x => _ + (x * _)) h_exists.symm
+      _ = w*z + (w*y + a*y) := congrArg (_ + .) (Natural.right_distributive _ _ _)
+      _ = (w*z + w*y) + a*y := (Natural.add_associative _ _ _).symm
+      _ = (w*y + w*z) + a*y := congrArg (. + _) (Natural.add_commutative _ _)
+    have h₂ : a*z = a*y := Natural.add_left_cancel h₁
+    have h₃ : z = y := Natural.multiply_left_cancel h₂ h_positive
+    calc
+      y + 0 = y := Natural.add_zero _
+      _ = z := h₃.symm
+      _ = 0 + z := (Natural.zero_add _).symm }
+  cases Natural.less_than_trichotomous n m with
+  | inl h_less =>
+    apply Or.inr
+    apply Quotient.sound
+    exact f h_less h_equivalent
+  | inr h_other => cases h_other with
+    | inl h_equal => 
+      apply Or.inl
+      apply Quotient.sound
+      calc
+        n + 0 = n := Natural.add_zero n
+        _ = m := h_equal
+        _ = 0 + m := (Natural.zero_add m).symm
+    | inr h_greater =>
+      apply Or.inr
+      apply Quotient.sound
+      apply f h_greater 
+      calc
+        m*k + n*l = n*l + m*k := Natural.add_commutative _ _
+        _ = n*k + m*l := h_equivalent.symm
+        _ = m*l + n*k := Natural.add_commutative _ _
 
+theorem Or.implies {a b c d : Prop} (f : a → c) (g : b → d) (h : a ∨ b) : c ∨ d := sorry
   
 theorem multiply_equal_zero_of_equal_zero : ∀ {a b : ℤ}, a = 0 ∨ b = 0 → a * b = 0 := by
-  sorry
+  apply Quotient.ind₂
+  intro (n, m) (k, l) h_quotient
+  suffices ∀ {w x y z : ℕ}, (h_zero : Quotient.mk' (w, x) = (0 : ℤ)) → multiply (Quotient.mk' (w, x)) (Quotient.mk' (y, z)) = 0 by
+  { cases h_quotient with
+    | inl h_nm => exact this h_nm
+    | inr h_kl => exact (multiply_commutative _ _).trans (this h_kl) }
+  intro w x y z h_zero
+  apply Quotient.sound
+  have h_equal := calc
+    w = w + 0 := (Natural.add_zero _).symm
+    _ = 0 + x := Quotient.exact h_zero
+    _ = x := Natural.zero_add _
+  have h_equivalent := calc
+    w*y + x*z = x*z + w*y := Natural.add_commutative _ _
+    _ = w*z + w*y := congrArg (λ x => (x * _) + _) h_equal.symm
+    _ = w*z + x*y := congrArg (λ x => _ + (x * _)) h_equal
+  calc
+    (w*y + x*z) + 0 = w*y + x*z := Natural.add_zero _
+    _ = w*z + x*y := h_equivalent
+    _ = 0 + (w*z + x*y) := (Natural.zero_add _).symm
   
-theorem multiply_left_cancel : ∀ {a b c : ℤ}, a * b = a * c → a ≠ 0 → b = c := by
-  sorry
-  
-theorem multiply_right_cancel : ∀ {a b c : ℤ}, a * c = b * c → c ≠ 0 → a = b := by
-  sorry
+theorem Or.resolve_left {a b : Prop} (h : a ∨ b) (not_a : ¬a) : b :=
+  match h with
+  | Or.inl ha => absurd ha not_a
+  | Or.inr hb => hb
+
+-- Looked at hints, attempted a lifting proof and that's probably way overkill
+theorem multiply_left_cancel {a b c : ℤ} (h_equivalent :  a * b = a * c) (h_not_equal_zero : a ≠ 0) : b = c := by
+  have := calc
+    0 = a*c - a*c := (add_inverse (a*c)).symm
+    _ = a*c - a*b := congrArg (_ - .) h_equivalent.symm
+    _ = a*c + -(a*b) := rfl
+    _ = a*c + a*(-b) := congrArg (_ + .) (negate_multiply_equal_multiply_negate a b)
+    _ = a*(c - b) := (left_distributive _ _ _).symm
+  have : a = 0 ∨ c - b = 0 := equal_zero_of_multiply_equal_zero this.symm
+  have : c - b = 0 := Or.resolve_left this h_not_equal_zero
+  show b = c
+  exact (equal_of_subtract_equal_zero this).symm
+
+theorem multiply_right_cancel {a b c : ℤ} (h_equivalent : a * c = b * c) (h_not_equal_zero : c ≠ 0) : a = b := by
+  have := calc
+    c * a = a * c := multiply_commutative _ _
+    _ = b * c := h_equivalent
+    _ = c * b := multiply_commutative _ _
+  exact multiply_left_cancel this h_not_equal_zero
 
 -- TODO: Need to either define HAdd ℤ ℕ, or look into casting. There's cons to both but I think casting might be the way
 -- def lessThan (a b : ℤ) : Prop := ∃ (n : ℕ), a + (cast n) = b
