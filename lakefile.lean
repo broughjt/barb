3f84460e06b86de4d8f067a77f9172c58d9b4898
Jackson Brough <jacksontbrough@gmail.com>
2025-11-29 17:28:53 -0700

    Nuke the Lean code

--- lakefile.lean
+++
@@ -1,9 +0,0 @@
-import Lake
-open Lake DSL
-
-package «Barb» where
-  -- add package configuration options here
-
-@[default_target]
-lean_lib «Barb» where
-  -- add library configuration options here
